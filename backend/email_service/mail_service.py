aad5bb294315eb1e42d55c4fd06d24a494b62b52
Andrew-Nzioki <andrewkyalonzioki001@gmail.com>
2024-09-15 15:32:19 +0300

    email service

---
+++ backend/email_service/mail_service.py
@@ -0,0 +1,38 @@
+import resend
+from pydantic import BaseModel
+import os
+from typing import Annotated
+from fastapi import Depends
+
+
+resend.api_key = os.environ.get("EMAIL_SERVICE_API_KEY")
+EMAIL_DOMAIN = os.environ.get("EMAIL_DOMAIN")
+SENDER_ADDRESS = f"donotreply@{EMAIL_DOMAIN}"
+
+
+class SendEmailParams(BaseModel):
+    email: str
+    subject: str
+    message: str
+
+def send_mail(body: SendEmailParams) -> resend.Email:
+    params = {
+        "from": SENDER_ADDRESS,
+        "to": [body.email],
+        "subject": body.subject,
+        "html": f"<strong>{body.message}</strong>",
+    }
+    email = resend.Emails.send(params)
+    return email
+
+
+class EmailService:
+    def send(self, body: SendEmailParams) -> resend.Email:
+        return send_mail(body)
+
+
+def get_email_service() -> EmailService:
+    return EmailService()
+
+
+EmailServiceDependency = Annotated[EmailService, Depends(get_email_service)]
