bb05ca5401a2dcc71807b1a6270dd6b6c45e4d84
Andrew-Nzioki <andrewkyalonzioki001@gmail.com>
2024-09-19 22:52:23 +0300

    commit

--- backend/student/student_controllers.py
+++ backend/student/student_controllers.py
@@ -3,40 +3,50 @@ import uuid
 from pydantic import BaseModel
 from fastapi import APIRouter, HTTPException
 from backend.database.database import DatabaseDependency
-from backend.models import Classroom, ParentStudentAssociation, SchoolParent, Student, User
+from backend.models import (
+    Classroom,
+    ParentStudentAssociation,
+    SchoolParent,
+    Student,
+    User,
+)
 from backend.user.user_authentication import UserAuthenticationContextDependency
+
 router = APIRouter()
 
 
 class CreateStudent(BaseModel):
     first_name: str
     last_name: str
-    parent_id:str
+    parent_id: uuid.UUID
     date_of_birth: datetime.datetime
     gender: str
     grade_level: int
-    classroom_id:uuid.UUID
-
+    classroom_id: uuid.UUID
 
 
 @router.post("/students/create")
-async def create_student(db:DatabaseDependency,body: CreateStudent,auth_context: UserAuthenticationContextDependency):
-    
-    user= db.query(User).filter(User.id == auth_context.user_id).first()
+async def create_student(
+    db: DatabaseDependency,
+    body: CreateStudent,
+    auth_context: UserAuthenticationContextDependency,
+):
+
+    user = db.query(User).filter(User.id == auth_context.user_id).first()
 
     if not user:
         raise HTTPException(status_code=403)
-    
+
     parent = db.query(SchoolParent).filter(SchoolParent.id == body.parent_id).first()
 
-    if not parent :
+    if not parent:
         raise HTTPException(status_code=404, detail="Parent  not found")
-    
-    class_room=db.query(Classroom).filter(Classroom.id == body.classroom_id).first()
+
+    class_room = db.query(Classroom).filter(Classroom.id == body.classroom_id).first()
 
     if not class_room:
         raise HTTPException(status_code=404, detail="Classroom not found")
-    #--
+    # --
     if not any(
         permission.permissions.student_permissions.can_add_students
         for role in user.roles
@@ -44,23 +54,28 @@ async def create_student(db:DatabaseDependency,body: CreateStudent,auth_context:
         for permission in role.user_permissions
     ):
         raise HTTPException(status_code=403, detail="Permission denied")
-
+    # ---
+    student_user = User(
+        email=body.first_name + body.last_name + "@student.com",
+        username=body.first_name + body.last_name,
+        password_hash="student",
+    )
+    db.add(student_user)
     student = Student(
         first_name=body.first_name,
         last_name=body.last_name,
         date_of_birth=body.date_of_birth,
         gender=body.gender,
         grade_level=body.grade_level,
-        classroom_id=class_room.id
-
+        classroom_id=class_room.id,
+        user_id=student_user.id,
     )
 
     db.add(student)
     db.flush()
     # ---
     parent_student_association = ParentStudentAssociation(
-        parent_id=body.parent_id,
-        student_id=student.id
+        parent_id=body.parent_id, student_id=student.id
     )
     db.add(parent_student_association)
     db.flush()
@@ -68,13 +83,18 @@ async def create_student(db:DatabaseDependency,body: CreateStudent,auth_context:
 
     return {}
 
+
 @router.get("/students/{student_id}")
-async def get_student(db:DatabaseDependency,student_id:uuid.UUID,auth_context: UserAuthenticationContextDependency):
-    user= db.query(User).filter(User.id == auth_context.user_id).first()
+async def get_student(
+    db: DatabaseDependency,
+    student_id: uuid.UUID,
+    auth_context: UserAuthenticationContextDependency,
+):
+    user = db.query(User).filter(User.id == auth_context.user_id).first()
 
     if not user:
         raise HTTPException(status_code=403)
-    
+
     student = db.query(Student).filter(Student.id == student_id).first()
 
     if not student:
@@ -90,14 +110,19 @@ async def get_student(db:DatabaseDependency,student_id:uuid.UUID,auth_context: U
 
     return student
 
-@router.get("/students/{student_id}/parents")
-async def get_student_parents(db:DatabaseDependency,student_id:uuid.UUID,auth_context: UserAuthenticationContextDependency):
 
-    user= db.query(User).filter(User.id == auth_context.user_id).first()
+@router.get("/students/{student_id}/parents")
+async def get_student_parents(
+    db: DatabaseDependency,
+    student_id: uuid.UUID,
+    auth_context: UserAuthenticationContextDependency,
+):
+
+    user = db.query(User).filter(User.id == auth_context.user_id).first()
 
     if not user:
         raise HTTPException(status_code=403)
-    
+
     student = db.query(Student).filter(Student.id == student_id).first()
 
     if not student:
@@ -110,7 +135,7 @@ async def get_student_parents(db:DatabaseDependency,student_id:uuid.UUID,auth_co
         for permission in role.user_permissions
     ):
         raise HTTPException(status_code=403, detail="Permission denied")
-    
+
     if not any(
         permission.permissions.parent_permissions.can_view_parents
         for role in user.roles
@@ -119,18 +144,28 @@ async def get_student_parents(db:DatabaseDependency,student_id:uuid.UUID,auth_co
     ):
         raise HTTPException(status_code=403, detail="Permission denied")
 
-    parents = db.query(SchoolParent).join(ParentStudentAssociation).filter(ParentStudentAssociation.student_id == student_id).all()
+    parents = (
+        db.query(SchoolParent)
+        .join(ParentStudentAssociation)
+        .filter(ParentStudentAssociation.student_id == student_id)
+        .all()
+    )
 
     return parents
 
-@router.get("/students/{student_id}/classroom")
-async def get_student_classroom(db:DatabaseDependency,student_id:uuid.UUID,auth_context: UserAuthenticationContextDependency):
 
-    user= db.query(User).filter(User.id == auth_context.user_id).first()
+@router.get("/students/{student_id}/classroom")
+async def get_student_classroom(
+    db: DatabaseDependency,
+    student_id: uuid.UUID,
+    auth_context: UserAuthenticationContextDependency,
+):
+
+    user = db.query(User).filter(User.id == auth_context.user_id).first()
 
     if not user:
         raise HTTPException(status_code=403)
-    
+
     student = db.query(Student).filter(Student.id == student_id).first()
 
     if not student:
@@ -143,18 +178,23 @@ async def get_student_classroom(db:DatabaseDependency,student_id:uuid.UUID,auth_
         for permission in role.user_permissions
     ):
         raise HTTPException(status_code=403, detail="Permission denied")
-    
+
     classroom = db.query(Classroom).filter(Classroom.id == student.classroom_id).first()
 
     return classroom
 
+
 @router.get("/students/{class_room_id}")
-async def get_students_in_classroom(db:DatabaseDependency,class_room_id:uuid.UUID,auth_context: UserAuthenticationContextDependency):
-    user= db.query(User).filter(User.id == auth_context.user_id).first()
+async def get_students_in_classroom(
+    db: DatabaseDependency,
+    class_room_id: uuid.UUID,
+    auth_context: UserAuthenticationContextDependency,
+):
+    user = db.query(User).filter(User.id == auth_context.user_id).first()
 
     if not user:
         raise HTTPException(status_code=403)
-    
+
     class_room = db.query(Classroom).filter(Classroom.id == class_room_id).first()
 
     if not class_room:
--- environment_variables.md
+++ environment_variables.md
@@ -18,16 +18,4 @@ DATABASE_USER="postgres"
 DATABASE_PASSWORD="postgres"
 
 
-## MinIO Configuration(local s3)
-MINIO_ACCESS_KEY=minioadmin
-MINIO_SECRET_KEY=minioadmin
-MINIO_BUCKET_NAME=main
 
-## local Mail server
-
-MAIL_SERVER_HOST="localhost"
-MAIL_SERVER_PORT="1025"
-MAIL_SERVER_USERNAME=""
-MAIL_SERVER_PASSWORD=""
-MAIL_USE_TLS=False
-MAIL_USE_SSL=False
