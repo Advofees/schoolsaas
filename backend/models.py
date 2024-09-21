295c1f60d4a7c775557f5e1f3885149aebf09301
Andrew-Nzioki <andrewkyalonzioki001@gmail.com>
2024-09-21 17:37:47 +0300

    commit

--- backend/models.py
+++ backend/models.py
@@ -347,12 +347,41 @@ class Module(Base):
         "Teacher", secondary="teacher_module_association", back_populates="modules"
     )
     exams: Mapped[list["Exam"]] = relationship("Exam", back_populates="module")
+     
+
+    module_enrollments: Mapped[list["ModuleEnrollment"]] = relationship(
+        "ModuleEnrollment", back_populates="module"
+    )
+
+    students: Mapped[list["Student"]] = relationship(
+        "Student",
+        secondary="enrollments",
+        back_populates="modules",
+        viewonly=True,
+    )
 
     def __init__(self, name: str, description: typing.Optional[str] = None):
         super().__init__()
         self.name = name
         self.description = description
 
+class ModuleEnrollment(Base):
+    __tablename__ = "enrollments"
+
+    student_id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), ForeignKey("students.id"), primary_key=True
+    )
+    module_id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), ForeignKey("modules.id"), primary_key=True
+    )
+
+    student: Mapped["Student"] = relationship("Student", back_populates="enrollments")
+    module: Mapped["Module"] = relationship("Module", back_populates="enrollments")
+
+    def __init__(self, student_id:uuid.UUID, module_id:uuid.UUID):
+        super().__init__()
+        self.student_id = student_id
+        self.module_id = module_id
 
 class Attendance(Base):
     __tablename__ = "attendances"
