71587ac0077ae64c924bb82653903d4bda714115
Andrew-Nzioki <andrewkyalonzioki001@gmail.com>
2024-12-17 20:36:34 +0300

    get school classrooms

--- backend/classroom/classroom_controller.py
+++ backend/classroom/classroom_controller.py
@@ -1,8 +1,8 @@
 import uuid
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, status, Query
 from pydantic import BaseModel
 from backend.database.database import DatabaseDependency
-from backend.user.user_models import User
+from backend.user.user_models import RoleType, User
 from backend.user.user_authentication import UserAuthenticationContextDependency
 from backend.teacher.teacher_model import ClassTeacherAssociation, Teacher
 from backend.classroom.classroom_model import Classroom
@@ -10,11 +10,41 @@ from backend.classroom.classroom_model import Classroom
 router = APIRouter()
 
 
+@router.get("/classrooms/list")
+def school_classrooms(
+    db: DatabaseDependency,
+    auth_context: UserAuthenticationContextDependency,
+    limit: int = Query(default=10, ge=1),
+    offset: int = Query(default=0, ge=0),
+):
+    user = db.query(User).filter(User.id == auth_context.user_id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+
+    if not (
+        user.has_role_type(RoleType.SUPER_ADMIN)
+        or user.has_role_type(RoleType.CLASS_TEACHER)
+        or user.has_role_type(RoleType.TEACHER)
+    ):
+
+        raise HTTPException(status_code=403, detail="permission-denied")
+
+    classrooms = (
+        db.query(Classroom)
+        .filter(Classroom.school_id == user.school_id)
+        .offset(offset)
+        .limit(limit)
+        .all()
+    )
+
+    return classrooms
+
+
 class classTeacherAssociation(BaseModel):
     is_primary: bool
 
 
-@router.post("/classroom/{classrom_id}/teachers/{teacher_id}/associate")
+@router.post("/classroom/{classrom_id}/teachers/{teacher_id}/add")
 def assign_teacher_to_classroom(
     db: DatabaseDependency,
     auth_context: UserAuthenticationContextDependency,
@@ -24,15 +54,15 @@ def assign_teacher_to_classroom(
 ):
     user = db.query(User).filter(User.id == auth_context.user_id).first()
     if not user:
-        raise HTTPException(404)
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
 
     classroom = db.query(Classroom).filter(Classroom.id == classroom_id).first()
     if not classroom:
-        raise HTTPException(404)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
     teacher = db.query(Teacher).filter(Teacher.id == teacher_id).first()
     if not teacher:
-        raise HTTPException(404)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
     association = ClassTeacherAssociation(
         teacher_id=teacher.id, classroom_id=classroom.id, is_primary=body.is_primary
